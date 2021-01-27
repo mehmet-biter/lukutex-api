50d825bb9d497cfa0f6b77c841cf4c0de57a1464
Phuoc Huynh <huynhphvan@gmail.com>
2021-01-27 20:52:45 +0700

    update

--- src/trading-competition/controllers/competitions.js
+++ src/trading-competition/controllers/competitions.js
@@ -1,11 +1,4 @@
-const MarketsModel = require('../../../models/Markets');
-
 const CompetitionsModel = require('../models/Competitions');
-
-const covertMarketID = async(market_id) => {
-    const units = await MarketsModel.getBaseAndQuoteUnit(market_id);
-    return Promise.resolve(units[0][0].base_unit + '/' + units[0][0].quote_unit);
-}
 
 exports.fetchAllCompetitions = async(req, res, next) => {
     try {
@@ -13,50 +6,36 @@ exports.fetchAllCompetitions = async(req, res, next) => {
         const upcomingCompetitions = await CompetitionsModel.fetchUpcomingCompetitions();
         const endedCompetitions = await CompetitionsModel.fetchEndedCompetitions();
 
-        const mappedOngoingCompetitions = await Promise.all(ongoingCompetitions[0].map(async(competition) => {
-            const marketIds = competition.market_ids.replace(/ /g, '').split('-');
-            const convertedMarketIds = await Promise.all(marketIds.map(async(market_id) => {
-                const newMarketID = await covertMarketID(market_id);
-                return newMarketID;
-            }));
-            const newCompetition = {
+        const newOngoingCompetitions = ongoingCompetitions[0].map(competition => {
+            const newComp = {
                 ...competition,
-                market_ids: convertedMarketIds
+                market_ids: competition.market_ids.replace(/ /g, '').split(',')
             }
-            return newCompetition;
-        }));
+            return newComp;
+        });
 
-        const mappedUpcomingCompetitions = await Promise.all(upcomingCompetitions[0].map(async(competition) => {
-            const marketIds = competition.market_ids.replace(/ /g, '').split('-');
-            const convertedMarketIds = await Promise.all(marketIds.map(async(market_id) => {
-                const newMarketID = await covertMarketID(market_id);
-                return newMarketID;
-            }));
-            const newCompetition = {
+        const newUpcomingCompetitions = upcomingCompetitions[0].map(competition => {
+            const newComp = {
                 ...competition,
-                market_ids: convertedMarketIds
+                market_ids: competition.market_ids.replace(/ /g, '').split(',')
             }
-            return newCompetition;
-        }));
+            return newComp;
+        })
 
-        const mappedEndedCompetitions = await Promise.all(endedCompetitions[0].map(async(competition) => {
-            const marketIds = competition.market_ids.replace(/ /g, '').split('-');
-            const convertedMarketIds = await Promise.all(marketIds.map(async(market_id) => {
-                const newMarketID = await covertMarketID(market_id);
-                return newMarketID;
-            }));
-            const newCompetition = {
+        const newEndedCompetitions = endedCompetitions[0].map(competition => {
+            const newComp = {
                 ...competition,
-                market_ids: convertedMarketIds
+                market_ids: competition.market_ids.replace(/ /g, '').split(',')
             }
-            return newCompetition;
-        }));
+            return newComp;
+        })
+
         res.status(200).json({
             msg: "Fetch all competitions successfully.",
             payload: {
-                ongoing: mappedOngoingCompetitions,
-                upcoming: mappedUpcomingCompetitions,
-                ended: mappedEndedCompetitions
+                ongoing: newOngoingCompetitions,
+                upcoming: newUpcomingCompetitions,
+                ended: newEndedCompetitions
             }
         })
     } catch (error) {
@@ -68,101 +47,22 @@ exports.fetchAllCompetitions = async(req, res, next) => {
     }
 }
 
-
-exports.fetchOngoingCompetitions = async(req, res, next) => {
-    try {
-        const activeCompetitions = await CompetitionsModel.fetchOngoingCompetitions();
-
-        const mappedActiveCompetitions = await Promise.all(activeCompetitions[0].map(async(competition) => {
-            const marketIds = competition.market_ids.replace(/ /g, '').split('-');
-            const convertedMarketIds = await Promise.all(marketIds.map(async(market_id) => {
-                const newMarketID = await covertMarketID(market_id);
-                return newMarketID;
-            }));
-            const newCompetition = {
-                ...competition,
-                market_ids: convertedMarketIds
-            }
-            return newCompetition;
-        }));
-        res.status(200).json({
-            msg: "Fetch active competitions successfully.",
-            payload: [...mappedActiveCompetitions]
-        })
-    } catch (error) {
-        console.log(error);
-        res.status(404).json({
-            msg: 'Fetch active competitions failed',
-            payload: []
-        })
-    }
-}
-
-exports.fetchUpcomingCompetitions = async(req, res, next) => {
-    try {
-        const upcomingCompetitions = await CompetitionsModel.fetchActiveCompetitions();
-
-        const mappedUpcomingCompetitions = await Promise.all(upcomingCompetitions[0].map(async(competition) => {
-            const marketIds = competition.market_ids.replace(/ /g, '').split('-');
-            const convertedMarketIds = await Promise.all(marketIds.map(async(market_id) => {
-                const newMarketID = await covertMarketID(market_id);
-                return newMarketID;
-            }));
-            const newCompetition = {
-                ...competition,
-                market_ids: convertedMarketIds
-            }
-            return newCompetition;
-        }));
-        res.status(200).json({
-            msg: "Fetch upcoming competitions successfully.",
-            payload: [...mappedUpcomingCompetitions]
-        })
-    } catch (error) {
-        console.log(error);
-        res.status(404).json({
-            msg: 'Fetch upcoming competitions failed',
-            payload: []
-        })
-    }
-}
-
-exports.fetchEndedCompetitions = async(req, res, next) => {
-    try {
-        const endedCompetitions = await CompetitionsModel.fetchActiveCompetitions();
-
-        const mappedEndedCompetitions = await Promise.all(endedCompetitions[0].map(async(competition) => {
-            const marketIds = competition.market_ids.replace(/ /g, '').split('-');
-            const convertedMarketIds = await Promise.all(marketIds.map(async(market_id) => {
-                const newMarketID = await covertMarketID(market_id);
-                return newMarketID;
-            }));
-            const newCompetition = {
-                ...competition,
-                market_ids: convertedMarketIds
-            }
-            return newCompetition;
-        }));
-        res.status(200).json({
-            msg: "Fetch ended competitions successfully.",
-            payload: [...mappedEndedCompetitions]
-        })
-    } catch (error) {
-        console.log(error);
-        res.status(404).json({
-            msg: 'Fetch ended competitions failed',
-            payload: []
-        })
-    }
-}
-
 exports.fetchCompetitionById = async(req, res, next) => {
     const competition_id = req.params.competition_id;
     try {
         const competition = await CompetitionsModel.fetchCompetitionById(competition_id);
+
+        const newCompetition = competition[0].map(competition => {
+            const newComp = {
+                ...competition,
+                market_ids: competition.market_ids.replace(/ /g, '').split(',')
+            }
+            return newComp;
+        })
+
         res.status(200).json({
             msg: "Fetch competition with id successfully.",
-            payload: competition[0][0]
+            payload: newCompetition[0]
         })
     } catch (error) {
         console.log(error);
--- src/trading-competition/router/competitions.js
+++ src/trading-competition/router/competitions.js
@@ -4,9 +4,9 @@ const router = express.Router();
 const competitionsController = require("../controllers/competitions");
 
 router.get("/fetch/all", competitionsController.fetchAllCompetitions);
-router.get("/fetch/ongoing", competitionsController.fetchOngoingCompetitions);
-router.get("/fetch/upcoming", competitionsController.fetchUpcomingCompetitions);
-router.get("/fetch/ended", competitionsController.fetchEndedCompetitions);
+// router.get("/fetch/ongoing", competitionsController.fetchOngoingCompetitions);
+// router.get("/fetch/upcoming", competitionsController.fetchUpcomingCompetitions);
+// router.get("/fetch/ended", competitionsController.fetchEndedCompetitions);
 
 router.get("/fetch/competition_id=:competition_id", competitionsController.fetchCompetitionById);
 
