358dbda3b7eb35d660efd6c84a9b751055bae297
Phuoc Huynh <huynhphvan@gmail.com>
2021-01-27 12:01:44 +0700

    update

--- src/trading-competition/router/competitions.js
+++ src/trading-competition/router/competitions.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const competitionsController = require("../controllers/competitions");
 
-router.get("/fetch/active", competitionsController.fetchActiveCompetitions);
+router.get("/fetch/all", competitionsController.fetchAllCompetitions);
 router.get("/fetch/ongoing", competitionsController.fetchOngoingCompetitions);
 router.get("/fetch/upcoming", competitionsController.fetchUpcomingCompetitions);
 router.get("/fetch/ended", competitionsController.fetchEndedCompetitions);
