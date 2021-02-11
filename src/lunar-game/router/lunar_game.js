351bd41d2c87f49a842e0d6b705e062bdc40ba3d
Phuoc Huynh <huynhphvan@gmail.com>
2021-02-11 15:53:07 +0700

    update

--- src/lunar-game/router/lunar_game.js
+++ src/lunar-game/router/lunar_game.js
@@ -7,6 +7,6 @@ const rewardController = require('../controllers/reward');
 
 router.get("/luckymoney/fetch", luckyMoneyController.fetchLuckyMoney);
 router.get("/luckylots/fetch/uid=:uid", luckyLotsController.fetchLuckyLots);
-router.post("/reward/post/uid=:uid&txid=:txid", rewardController.reward);
+// router.post("/reward/post/uid=:uid&txid=:txid", rewardController.reward);
 // router.delete("/reward/delete", rewardController.deleteAllHistory);
 module.exports = router;
