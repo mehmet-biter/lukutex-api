1f5da597299d3a2a08f355f027887c55908ae6e1
Phuoc Huynh <huynhphvan@gmail.com>
2021-03-02 14:03:18 +0700

    fix username database config

--- configs/env.js
+++ configs/env.js
@@ -1,9 +1,9 @@
 const MYSQL_ENV_HOST = '167.99.65.65';
-const MYSQL_ENV_USER = 'root';
+const MYSQL_ENV_USERNAME = 'root';
 const MYSQL_ENV_PASSWORD = 'Quy1407@';
 
 module.exports = {
-    MYSQL_ENV_USER,
+    MYSQL_ENV_USERNAME,
     MYSQL_ENV_HOST,
     MYSQL_ENV_PASSWORD
 }
